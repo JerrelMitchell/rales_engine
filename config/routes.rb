f2ce3a8067b2b4df2ff1a2d89792c1c861d9c5f5
manojpanta <manojpanta95@gmail.com>
2018-06-26 10:23:13 -0600

    added faker and serializer gem

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,9 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-<<<<<<< HEAD
       get 'merchants/find', to: 'merchants/search#show'
       get 'merchants/find_all', to: 'merchants/search#index'
       get 'merchants/random', to: 'merchants/random#show'
-      resources :merchants
-=======
       get 'items/find', to: 'items/search#show'
       get 'items/find_all', to: 'items/search#index'
       get 'items/random', to: 'items/random#show'
@@ -14,7 +11,6 @@ Rails.application.routes.draw do
       resources :merchants do
         resources :items, only: [:index]
       end
->>>>>>> a8cea1554875549bba0e191d90d0a1fa01634c9b
     end
   end
 end
