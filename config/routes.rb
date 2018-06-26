74fa9c75d1f8b9098fa0187ea1f57c8cc344c096
JerrelMitchell <jerrelmitchell@ymail.com>
2018-06-25 20:38:01 -0600

    add in routes for item record endpoints

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :api do
+    namespace :v1 do
+      get 'items/find', to: 'items/search#show'
+      get 'items/find_all', to: 'items/search#index'
+      get 'items/random', to: 'items/random#show'
+      resources :items, only: [:index, :show]
+      resources :merchants do
+        resources :items, only: [:index]
+      end
+    end
+  end
 end
