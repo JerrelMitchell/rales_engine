3d866caab08441f5934f10d60daed69aaaecb5c0
manojpanta <manojpanta95@gmail.com>
2018-06-25 18:03:22 -0600

    added merchants controller inside api/v1 folder

--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -1,9 +1,9 @@
 require 'rails_helper'
 describe "Items API" do
   it "sends a list of merchants" do
-    create_list(:merchants, 3)
+    create_list(:merchant, 3)
 
-    get '/api/v1/items'
+    get '/api/v1/merchants'
 
     expect(response).to be_successful
   end
