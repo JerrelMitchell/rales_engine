083bbcae569ebe661ed607ed8da68fdb7ee8e0bb
JerrelMitchell <jerrelmitchell@ymail.com>
2018-06-26 15:42:27 -0600

    add invoices record endpoints, routes, and testing

--- app/controllers/api/v1/invoice_items/random_controller.rb
+++ app/controllers/api/v1/invoice_items/random_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::InvoiceItems::RandomController < ApplicationController
   def show
-    render json: InvoiceItem.find(InvoiceItem.all.sample.id)
+    render json: InvoiceItem.all.sample
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,6 @@ Rails.application.routes.draw do
     namespace :v1 do
       get 'merchants/find',     to: 'merchants/search#show'
       get 'merchants/find_all', to: 'merchants/search#index'
-      get 'merchants/random', to: 'merchants/random#show'
-
-      get 'items/find', to: 'items/search#show'
       get 'merchants/random',   to: 'merchants/random#show'
       resources :merchants do
         resources :items, only: [:index]
@@ -19,24 +16,25 @@ Rails.application.routes.draw do
         get 'invoice_items', to: 'items/invoice_items#index'
       end
 
+      get 'customers/find',     to: 'customers/search#show'
+      get 'customers/find_all', to: 'customers/search#index'
+      get 'customers/random',   to: 'customers/random#show'
+      resources :customers, only: [:index, :show]
+
       get 'transactions/find',     to: 'transactions/search#show'
       get 'transactions/find_all', to: 'transactions/search#index'
-
-      get 'transactions/random', to: 'transactions/random#show'
-
-      get 'customers/find', to: 'customers/search#show'
-      get 'customers/find_all', to: 'customers/search#index'
-      get 'customers/random', to: 'customers/random#show'
-
-
       get 'transactions/random',   to: 'transactions/random#show'
       resources :transactions, only: [:index, :show]
-      resources :customers, only: [:index, :show]
 
       get 'invoice_items/find',     to: 'invoice_items/search#show'
       get 'invoice_items/find_all', to: 'invoice_items/search#index'
       get 'invoice_items/random',   to: 'invoice_items/random#show'
       resources :invoice_items, only: [:index, :show]
+
+      get 'invoices/find',     to: 'invoices/search#show'
+      get 'invoices/find_all', to: 'invoices/search#index'
+      get 'invoices/random',   to: 'invoices/random#show'
+      resources :invoices, only: [:index, :show]
     end
   end
 end
