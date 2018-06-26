9b94e9f394bde8651d902758699a1b00dc6dfffe
manojpanta <manojpanta95@gmail.com>
2018-06-25 18:17:54 -0600

    added test for individual merchant

--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -3,8 +3,25 @@ describe "Items API" do
   it "sends a list of merchants" do
     create_list(:merchant, 3)
 
-    get '/api/v1/merchants'
+    get '/api/v1/merchants.json'
+
+    merchants = JSON.parse(response.body)
+
 
     expect(response).to be_successful
+    expect(merchants.count).to eq(3)
+  end
+
+  it "sends a one specific merchant" do
+
+    id = create(:merchant).id
+
+    get "/api/v1/merchants/#{id}"
+
+    merchant = JSON.parse(response.body)
+
+
+    expect(response).to be_successful
+    expect(merchant["id"]).to eq(id)
   end
 end
