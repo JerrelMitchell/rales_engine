af84571f8b8eebd9efdc1e90c08dc192309d3340
manojpanta <manojpanta95@gmail.com>
2018-06-27 21:08:33 -0600

    added implementation to find favorite merchant of a customer

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,4 +1,14 @@
 class Customer < ApplicationRecord
   has_many :invoices
   has_many :transactions, through: :invoices
+
+  def favorite_merchant
+    Merchant.select('merchants.*, COUNT(invoices)as invoices_count')
+    .joins(invoices: [:transactions])
+    .where(transactions: {result: 'success'})
+    .order('invoices_count DESC')
+    .group(:id)
+    .limit(1)
+    .first
+  end
 end
